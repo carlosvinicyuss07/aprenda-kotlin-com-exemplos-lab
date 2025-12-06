49945c883eca5b3704e94ed73bfbf6769136ae2d
Carlos Vinícyus <153529080+carlosvinicyuss07@users.noreply.github.com>
2025-12-05 21:47:52 -0300

    Implementacao da minha abordagem para o desafio proposto

--- desafio.kt
+++ desafio.kt
@@ -1,21 +1,36 @@
 // [Template no Kotlin Playground](https://pl.kotl.in/WcteahpyN)
 
-enum class Nivel { BASICO, INTERMEDIARIO, DIFICIL }
+enum class Nivel { BASICO, INTERMEDIARIO, AVANCADO }
 
-class Usuario
+data class Usuario(val nome: String, val email: String)
 
-data class ConteudoEducacional(var nome: String, val duracao: Int = 60)
+data class ConteudoEducacional(val nome: String, val duracao: Int = 60, val nivelDoConteudo: Nivel)
 
-data class Formacao(val nome: String, var conteudos: List<ConteudoEducacional>) {
+data class Formacao(val nome: String, val conteudos: List<ConteudoEducacional>, val nivelDaFormacao: Nivel) {
 
     val inscritos = mutableListOf<Usuario>()
     
     fun matricular(usuario: Usuario) {
-        TODO("Utilize o parâmetro $usuario para simular uma matrícula (usar a lista de $inscritos).")
+        inscritos.add(usuario)
     }
 }
 
 fun main() {
-    TODO("Analise as classes modeladas para este domínio de aplicação e pense em formas de evoluí-las.")
-    TODO("Simule alguns cenários de teste. Para isso, crie alguns objetos usando as classes em questão.")
+    val usuario1 = Usuario("Claúdio", "claudio.w@email.com")
+    val usuario2 = Usuario("José", "jose.fxp@email.com")
+    val usuario3 = Usuario("Maria", "m123.santos@email.com")
+    
+    val matematica1 = ConteudoEducacional(nome = "Matematica I", nivelDoConteudo = Nivel.BASICO)
+    val matematica2 = ConteudoEducacional(nome = "Matematica II", nivelDoConteudo = Nivel.INTERMEDIARIO)
+    val matematica3 = ConteudoEducacional(nome = "Matematica III", nivelDoConteudo = Nivel.AVANCADO)
+    
+    val listaDeConteudos = listOf(matematica1, matematica2, matematica3)
+    
+    val formacao = Formacao("Licenciatura em Matemática", listaDeConteudos, Nivel.AVANCADO)
+    formacao.matricular(usuario1)
+    formacao.matricular(usuario2)
+    formacao.matricular(usuario3)
+    
+    println(formacao.inscritos)
+    
 }
